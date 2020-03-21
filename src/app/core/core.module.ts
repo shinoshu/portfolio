2df78c27b6d080a598f3a61477fb77e130cb2198
Shuma Shinozaki <shu.shinozaki@gmail.com>
2020-03-21 14:40:30 +0900

    Update core.module.ts

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
-
+import { AppComponent } from '@app/core/containers';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [AppComponent],
+  imports: [CommonModule, RouterModule],
 })
-export class CoreModule { }
+export class CoreModule {}
