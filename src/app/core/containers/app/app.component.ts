96a66b470b71136f95e806fda7cf244808d6d1cf
Shuma Shinozaki <shu.shinozaki@gmail.com>
2020-03-22 00:36:11 +0900

    Update AppComponent

--- src/app/core/containers/app/app.component.ts
+++ src/app/core/containers/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+import { ContactDialogComponent } from '@app/pages/components';
+import { DialogService } from '@app/core/services';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'portfolio';
+  constructor(private dialogService: DialogService) {}
+
+  openDialog() {
+    this.dialogService.openDialog(ContactDialogComponent);
+  }
 }
