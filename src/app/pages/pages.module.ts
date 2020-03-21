1602ecdfe6a346f2149b9164f68b469c1d9cadfd
Shuma Shinozaki <shu.shinozaki@gmail.com>
2020-03-21 16:13:51 +0900

    Update pages.module.ts

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { PagesRoutingModule } from '@app/pages/pages-routing.module';
+
 import { ProfileComponent } from '@app/pages/containers';
 
 @NgModule({
   declarations: [ProfileComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, PagesRoutingModule],
 })
 export class PagesModule {}
