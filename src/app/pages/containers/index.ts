154878161df50a04889d3752a2900ed8ea1be915
Shuma Shinozaki <shu.shinozaki@gmail.com>
2020-03-21 20:16:50 +0900

    Update pages-routing.module.ts, pages/containers/index.ts

--- src/app/pages/containers/index.ts
+++ src/app/pages/containers/index.ts
@@ -1,2 +1,3 @@
 export * from './profile/profile.component';
+export * from './skills/skills.component';
 export * from './work-list/work-list.component';
--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ProfileComponent } from '@app/pages/containers';
+import { SkillsComponent } from '@app/pages/containers';
 import { WorkListComponent } from '@app/pages/containers';
 
 const routes: Routes = [
   {
     path: 'profile',
     component: ProfileComponent,
+  },
+  {
+    path: 'skills',
+    component: SkillsComponent,
   },
   {
     path: 'work',
