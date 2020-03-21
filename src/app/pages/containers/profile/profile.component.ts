d8b41a1f1190c8b4415d66f449d44762658a2e4d
Shuma Shinozaki <shu.shinozaki@gmail.com>
2020-03-21 23:34:59 +0900

    Update ProfileComponent

--- src/app/pages/containers/profile/profile.component.ts
+++ src/app/pages/containers/profile/profile.component.ts
@@ -1,24 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss'],
 })
-export class ProfileComponent implements OnInit {
-  firstFormGroup?: FormGroup;
-  secondFormGroup?: FormGroup;
-
-  constructor(private formBuilder: FormBuilder) {}
-
-  ngOnInit() {
-    this.firstFormGroup = this.formBuilder.group({
-      firstName: ['Shuma', Validators.required],
-      lastName: ['Shinozaki', Validators.required],
-    });
-    this.secondFormGroup = this.formBuilder.group({
-      email: ['', Validators.required],
-    });
-  }
-}
+export class ProfileComponent {}
