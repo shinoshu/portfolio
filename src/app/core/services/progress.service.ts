c686ce29c849be9c06b422947b539f3b9c517240
Shuma Shinozaki <shu.shinozaki@gmail.com>
2020-03-21 22:50:39 +0900

    Update progress.service.ts

--- src/app/core/services/progress.service.ts
+++ src/app/core/services/progress.service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@angular/core';
+import { MatSpinner } from '@angular/material/progress-spinner';
+import { Overlay, OverlayRef } from '@angular/cdk/overlay';
+import { ComponentPortal } from '@angular/cdk/portal';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProgressService {
-  constructor() {}
+  overlayRef: OverlayRef;
+
+  constructor(private overlay: Overlay) {
+    this.overlayRef = this.overlay.create({
+      hasBackdrop: true,
+      positionStrategy: this.overlay
+        .position()
+        .global()
+        .centerHorizontally()
+        .centerVertically(),
+    });
+  }
+
+  showProgress() {
+    this.overlayRef.attach(new ComponentPortal(MatSpinner));
+  }
+
+  hideProgress() {
+    this.overlayRef.detach();
+  }
 }
