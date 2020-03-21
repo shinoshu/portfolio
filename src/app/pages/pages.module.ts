58a5ce89686c27dcabc95175442755ba0fb408db
Shuma Shinozaki <shu.shinozaki@gmail.com>
2020-03-21 18:03:03 +0900

    Update pages.module.ts

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
+import { MaterialModule } from '@app/material';
 import { PagesRoutingModule } from '@app/pages/pages-routing.module';
 
 import { ProfileComponent } from '@app/pages/containers';
@@ -8,6 +10,6 @@ import { WorkComponent } from './containers/work/work.component';
 
 @NgModule({
   declarations: [ProfileComponent, WorkComponent],
-  imports: [CommonModule, PagesRoutingModule],
+  imports: [CommonModule, FlexLayoutModule, MaterialModule, PagesRoutingModule],
 })
 export class PagesModule {}
