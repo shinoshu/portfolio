1e0989bc4f58be07063c4ea0b1f5e4365ecc875e
Shuma Shinozaki <shu.shinozaki@gmail.com>
2020-03-22 00:24:18 +0900

    Update dialog.service.ts

--- src/app/core/services/dialog.service.ts
+++ src/app/core/services/dialog.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ComponentType } from '@angular/cdk/portal';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DialogService {
-  constructor() {}
+  constructor(private matDialog: MatDialog) {}
+
+  openDialog<T>(component: ComponentType<T>) {
+    this.matDialog.open(component);
+  }
 }
