91cc1161b1b7ae837a5bb382e8cd4df50d7f2e99
Shuma Shinozaki <shu.shinozaki@gmail.com>
2020-03-21 17:59:42 +0900

    ng generate component work

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { PagesRoutingModule } from '@app/pages/pages-routing.module';
 
 import { ProfileComponent } from '@app/pages/containers';
+import { WorkComponent } from './containers/work/work.component';
 
 @NgModule({
-  declarations: [ProfileComponent],
+  declarations: [ProfileComponent, WorkComponent],
   imports: [CommonModule, PagesRoutingModule],
 })
 export class PagesModule {}
